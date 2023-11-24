13c58ab2be89e501df788bc401901a59393b17cb
Lachlan Williams <lwil0039@student.monash.edu>
2023-11-24 18:30:19 +1100

    screens implemented

--- circle/App.js
+++ circle/App.js
@@ -1,17 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView,Text, View } from 'react-native';
-import styles from './styles/styles';
-import NavBar from './components/navBar/navBar';
+// import { StatusBar } from 'expo-status-bar';
+// import { SafeAreaView,Text, View } from 'react-native';
+// import styles from './styles/styles';
+// import NavBar from './components/navBar/navBar';
 
 
-export default function App() {
-  return (
-    <View style={styles.purple}>
-      <SafeAreaView style={styles.safeArea}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <NavBar/>
-      <StatusBar style="auto" />
-      </SafeAreaView>
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.purple}>
+//       <SafeAreaView style={styles.safeArea}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <NavBar/>
+//       <StatusBar style="auto" />
+//       </SafeAreaView>
+//     </View>
+//   );
+// }
+
+import React from 'react';
+import AppNavigator from './AppNavigator';
+
+const App = () => {
+  return <AppNavigator />;
+};
+
+export default App;
