52fe3c28b9656a26044c3d7336f1c3ca4c42c147
Lachlan Williams <lwil0039@student.monash.edu>
2023-11-24 18:37:10 +1100

    ProfileScreen commonLayout

--- circle/screens/ProfileScreen.js
+++ circle/screens/ProfileScreen.js
@@ -1,11 +1,15 @@
+// ProfileScreen.js
 import React from 'react';
 import { View, Text } from 'react-native';
+import CommonLayout from './CommonLayout';
 
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>This is the Profile screen</Text>
-    </View>
+    <CommonLayout>
+      <View>
+        <Text>This is the Profile screen</Text>
+      </View>
+    </CommonLayout>
   );
 };
 
--- circle/styles/styles.js
+++ circle/styles/styles.js
@@ -1,9 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  // ... other styles
-
-  navBar: {
+   navBar: {
     height: 110,
     flexDirection: 'row',
     alignItems: 'center',
