ac79fb957e577167e6411ef7201d87ac944d67b0
Lachlan Williams <lwil0039@student.monash.edu>
2023-11-25 09:52:45 +1100

    Chat screen

---
+++ circle/screens/ChatScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import CommonLayout from './CommonLayout';
+
+const ChatScreen = () => {
+  return (
+    <CommonLayout>
+      <View>
+        <Text>This is the Chat screen</Text>
+      </View>
+    </CommonLayout>
+  );
+};
+
+export default ChatScreen;
