d282b02bbca9655cd1bffaf558e34ec692c5585d
Lachlan Williams <lwil0039@student.monash.edu>
2023-11-25 09:39:22 +1100

    premium screen

---
+++ circle/screens/PremiumScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import CommonLayout from './CommonLayout';
+
+const PremiumScreen = () => {
+  return (
+    <CommonLayout>
+      <View>
+        <Text>This is the Premium screen</Text>
+      </View>
+    </CommonLayout>
+  );
+};
+
+export default PremiumScreen;
