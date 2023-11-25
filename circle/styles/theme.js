1d8ffee36c7317714afbf81678fc11468dd857a3
Lachlan Williams <lwil0039@student.monash.edu>
2023-11-25 15:09:02 +1100

    Theme.js

---
+++ circle/styles/theme.js
@@ -0,0 +1,12 @@
+// theme.js
+export const colors = {
+    background: '#FDF7FA',
+    primary: '#FF0000',
+    secondary: '#FF008A',
+    text: '#000000',
+  };
+  
+  export const fonts = {
+    main: 'Zen Dots', // Replace with the correct font name
+  };
+  
